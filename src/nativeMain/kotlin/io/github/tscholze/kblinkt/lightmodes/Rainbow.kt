5eb6f5e3490674bcf6941f8b3a826cc05fc3141f
Tobias Scholze <t.scholze@anfe.ma>
2023-11-17 19:15:26 +0100

    Adjusted the rainbow light mode

--- src/nativeMain/kotlin/io/github/tscholze/kblinkt/lightmodes/Rainbow.kt
+++ src/nativeMain/kotlin/io/github/tscholze/kblinkt/lightmodes/Rainbow.kt
@@ -8,15 +8,15 @@ import io.github.tscholze.kblinkt.APA102
 fun APA102.rainbow() {
     setRgbForLed(0, 255, 255, 255)
 
-    setRgbForLed(0, 255, 0, 0)
-    setRgbForLed(0, 255, 0, 0)
+    setRgbForLed(1, 255, 0, 0)
+    setRgbForLed(2, 255, 0, 0)
 
-    setRgbForLed(0, 0, 255, 0)
-    setRgbForLed(0, 0, 255, 0)
+    setRgbForLed(3, 0, 255, 0)
+    setRgbForLed(4, 0, 255, 0)
 
-    setRgbForLed(0, 0, 0, 255)
-    setRgbForLed(0, 0, 0, 255)
+    setRgbForLed(5, 0, 0, 255)
+    setRgbForLed(7, 0, 0, 255)
 
-    setRgbForLed(0, 255, 255, 255)
+    setRgbForLed(7, 255, 255, 255)
 
 }
