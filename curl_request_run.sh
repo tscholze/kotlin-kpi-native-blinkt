731d4f47b77055ef438d5a3ec3ce630892b3c1a0
Tobias Scholze <5583469+tscholze@users.noreply.github.com>
2023-11-19 14:32:34 +0100

    Added Web, Morse and API feature

--- curl_request_run.sh
+++ curl_request_run.sh
@@ -2,3 +2,14 @@
 
 # Turn off all the things
 curl -X POST http://192.168.178.100:8080/on
+curl -X POST http://192.168.178.100:8080/off
+
+# Light mode sample
+# See `turnLightMode` for more supported ids
+curl -X POST http://192.168.178.100:8080/lightmode -d "blue"
+
+# Morse word
+curl -X POST http://192.168.178.100:8080/morse -d "hello"
+
+# System shutdown
+curl -X POST http://192.168.178.100:8080/shutdown
--- src/nativeMain/kotlin/io/github/tscholze/kblinkt/apa102/Command.kt
+++ src/nativeMain/kotlin/io/github/tscholze/kblinkt/apa102/Command.kt
@@ -10,4 +10,13 @@ sealed class Command {
 
     /** Turns all LEDs off which means setting a color of black */
     data object TurnOff : Command()
+
+    /** A custom lightmode has been requested */
+    data class LightMode(val id: String) : Command()
+
+    /** A morsing of given text has been requested */
+    data class Morse(val text: String) : Command()
+
+    /** Command to shutdown server and gpio and exit the program */
+    data object Shutdown : Command()
 }
